cee22bd90eed5bb171326bc29b34844df68e2d28
Kerekes Csaba <s18006@std.it-college.ac.jp>
2018-12-13 04:44:52 +0000

    kadai1212change

--- 1212/1212.1.sql
+++ 1212/1212.1.sql
@@ -1,2 +1,3 @@
 define specName = "'D%a%e%'"
-select last_name, first_name from employees where last_name like &specName;
+define specName2 = "'D%e%a%'"
+select last_name, first_name from employees where last_name like &specName or last_name like &specName2;
