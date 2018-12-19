151e36d44e2af3a4772773a0b2b215e9a171cc8f
Kerekes Csaba <s18006@std.it-college.ac.jp>
2018-12-19 00:46:27 +0000

    kadaichange1218.1.sh

--- 1218/1218.1.sql
+++ 1218/1218.1.sql
@@ -1,1 +1,1 @@
-select last_name "苗字", first_name "氏名", salary "給与", commission_pct "歩合" from employees where commission_pct >= .20;
+select last_name "苗字", first_name "氏名", salary "給与", commission_pct "歩合" from employees where commission_pct = .20;
